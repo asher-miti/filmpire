ecf465ae2ecc8f14e783bf2c68dfb25f2b017356
asher-miti <asher_3@hotmail.co.uk>
2022-06-10 21:30:22 +0100

    Component structure & routing setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+
+import { MovieInformation, Actors, Movies, Navbar, Profile } from './';
+
 const App = () => {
   return (
-   <div>hello world</div>
+    <div>
+      <CssBaseline />
+      <main>
+        <Switch>
+          <Route exact path='/' component={Movies} />
+          <Route exact path='/movie/:id' component={MovieInformation} />
+          <Route exact path='/actors/:id' component={Actors} />
+          <Route exact path='/profile/:id' component={Profile} />
+        </Switch>
+      </main>
+    </div>
   );
-}
+};
 
 export default App;
